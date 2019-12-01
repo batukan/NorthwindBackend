68fea9916f4b614eccb896c946ab0e7abce9dd26
Emre Batukan <batukan0emre@gmail.com>
2019-12-01 16:00:50 +0300

    Add project files.

---
+++ Business/Concrete/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductService(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            return new SuccessResult("product added successfully");
+        }
+
+        public IResult Delete(Product product)
+        {
+            _productDal.Delete(product);
+            return new SuccessResult("product deleted successfully");
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+        }
+
+        public IDataResult<List<Product>> GetList()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
+        }
+
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList(p => p.CategoryId == categoryId).ToList());
+        }
+
+        public IResult Update(Product product)
+        {
+            _productDal.Update(product);
+            return new SuccessResult("product updated successfully");
+        }
+    }
+}
