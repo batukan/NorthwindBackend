3e3d7656ff2d367d53f9a5bd414210189355cd13
Emre Batukan <batukan0emre@gmail.com>
2019-12-24 15:41:30 +0300

    Configure startup.cs

--- DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -12,7 +12,6 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
-
         }
 
         public DbSet<Product> Products { get; set; }
