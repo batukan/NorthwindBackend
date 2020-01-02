5aa0719ddf987059f364f64c4f24d5826c104b17
Emre Batukan <batukan0emre@gmail.com>
2020-01-02 22:07:49 +0300

    Begin memory cache

---
+++ Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,35 @@
+﻿namespace Core.CrossCuttingConcerns.Caching.Microsoft
+{
+    public class MemoryCacheManager:ICacheManager 
+    {
+        public T Get<T>(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Get(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(string key, object data, int duration)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool isAdded(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
